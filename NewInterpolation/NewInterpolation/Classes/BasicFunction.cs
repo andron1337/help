6141d346b0a485fc9f05c56050e451f6ec8368bb
andron1337 <45847854+andron1337@users.noreply.github.com>
2019-01-18 18:51:38 +0200

    Add files via upload

---
+++ NewInterpolation/NewInterpolation/Classes/BasicFunction.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewInterpolation
+{
+    public class BasicFunction
+    {
+        public float[] xTable;
+        public float[] yTable;
+        public float step, max;
+        public int size;               
+        public static int Amp;
+        public static int f;
+
+        public BasicFunction(int amp, int fr, float max, float step)
+        {
+            Amp = amp;
+            f = fr;
+            this.step = 0.1f;
+            this.max = max;
+
+            size = 1000;
+            xTable = new float[size];
+            yTable = new float[size];
+
+        }
+
+        public void BuildBasicFunction()
+        {
+            float j = 0;
+            for (int i = 0; i < size; i++, j += step)
+            {
+                xTable[i] = j;
+                yTable[i] = Func(j);
+            }
+        }
+
+        public static float Func(float x, float a = 3, float b = 6, float c = 10, float d = 15)
+        {
+            //var y = Amp * Math.Sin(2 * Math.PI * f * x);
+            a = f * a * 0.4f;
+            b = f * b * 0.4f;
+            c = f * c * 0.4f;
+            d = f * d * 0.4f;
+
+            float y = 0;
+            if (x < a)
+            {
+                y = 0;
+            }
+            else if (x >= a && x < b)
+            {
+                y = (x - a) / (b - a);
+            }
+            else if (x > b && x <= c)
+            {
+                y = 1;
+            }
+            else if (x > c && x <= d)
+            {
+                y = (d - x) / (d - c);
+            }
+            else if (x > d)
+            {
+                y = 0;
+            }
+            return y * Amp;
+        }
+
+        public static float calcSize(int freq)
+        {
+            float s = 0;
+            s = (freq >= 25) ? ((freq >= 100) ? (freq >= 1000 ? (freq >= 10000 ? 0.0001f : 0.001f) : 0.01f) : 0.1f) : 1;
+            return s;
+        }
+
+        public static float calcStep(int freq)
+        {
+            return 0.1f / freq;
+        }
+    }
+}
---
+++ NewInterpolation/NewInterpolation/Forms/MainForm.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace NewInterpolation
+{
+    public enum mode
+    {
+        linear,
+        bspline,
+        other
+    }
+    public partial class MainForm : Form
+    {
+        public BasicFunction bf;
+        public Sampler sm;
+        //public LinearInterpolator ln;
+        //public BsplineInterpolator bs;
+        public static int Amp, freq, sampling_f, linterp_f, binterp_f, scrollvalue;
+        public float time;
+        public mode ourmode;
+        public MainForm()
+        {
+            InitializeComponent();
+            label2.Text = "";
+            label4.Text = "";
+           
+            chart1.ChartAreas[0].AxisX.LabelStyle.Format = "{0:##0.###}u";
+        }
+
+        private void setup()
+        {
+
+            ourmode = mode.other;
+            BaseValues bv = new BaseValues();
+            bv.ShowDialog();
+
+            time = BasicFunction.calcSize(freq);
+            float step = BasicFunction.calcStep(freq);
+            bf = new BasicFunction(Amp, freq, time, step);
+            bf.BuildBasicFunction();
+
+            label2.Text = BasicFunction.Amp.ToString();
+            label4.Text = BasicFunction.f.ToString();
+
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+          
+                for (int i = 0; i < bf.size; i++)
+                    chart1.Series[0].Points.AddXY(bf.xTable[i], bf.yTable[i]);
+            
+            
+
+
+        }
+
+        private void sample()
+        {
+            if (freq == 0 || Amp == 0)
+            {
+                ErrorMessadge em = new ErrorMessadge();
+                em.ShowDialog();
+            }
+            else
+            {
+                ourmode = mode.other;
+                SamplingValues sv = new SamplingValues();
+                sv.setlabelandvalues("Частота дискретизації: ", MainForm.freq * 5, MainForm.freq * 100);
+                sv.ShowDialog();
+                sampling_f = sv.sampling_freq;
+                
+                sm = new Sampler(Amp, freq, sampling_f, time);
+                sm.Sampling();
+
+                chart1.Series[0].Points.Clear();
+                chart1.Series[1].Points.Clear();
+                chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StepLine;
+                for (int i = 0; i < sm.size; i++)
+                    chart1.Series[0].Points.AddXY(sm.x[i], sm.y[i]);
+                
+            }
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linear()
+        {
+            if (sampling_f == 0)
+            {
+                ErrorMessadge em = new ErrorMessadge();
+                em.ShowDialog();
+            }
+            else
+            {
+                SamplingValues sv = new SamplingValues();
+                sv.setlabelandvalues("Частота передискретизації: ", sampling_f * 2, sampling_f * 20);
+                sv.ShowDialog();
+                linterp_f = sv.sampling_freq;
+
+                ourmode = mode.linear;
+                //ln = new LinearInterpolator();
+                //ln.setup(sm.x, sm.y, sampling_f, sampling_f, time);
+                //ln.interpolation();
+                chart1.Series[0].Points.Clear();
+                chart1.Series[1].Points.Clear();
+                chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+                chart1.Series[1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StepLine;
+                //for (int i = 0; i < ln.size - 4; i++)
+                //    chart1.Series[0].Points.AddXY(ln.resX[i] + ln.step * 2, ln.resY[i]);
+                for (int i = 0; i < sm.size - 4; i++)
+                    chart1.Series[1].Points.AddXY(sm.x[i], sm.y[i]);
+
+               
+            }
+
+        }
+
+        private void bspline()
+        {
+            if (sampling_f == 0)
+            {
+                ErrorMessadge em = new ErrorMessadge();
+                em.ShowDialog();
+            }
+            else
+            {
+                SamplingValues sv = new SamplingValues();
+                sv.setlabelandvalues("Частота передискретизації: ", sampling_f * 2, sampling_f * 20);
+                sv.ShowDialog();
+                binterp_f = sv.sampling_freq;
+
+                ourmode = mode.bspline;
+                //bs = new BsplineInterpolator();
+                //bs.setup(sm.x, sm.y, sampling_f, sampling_f, time);
+                //bs.interpolation();
+                chart1.Series[0].Points.Clear();
+                chart1.Series[1].Points.Clear();
+                chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+                chart1.Series[1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StepLine;
+                //for (int i = 0; i < bs.size - 7; i++)
+                //    chart1.Series[0].Points.AddXY(bs.resX[i] + bs.step * 5, bs.resY[i]);
+                for (int i = 0; i < sm.size - 4; i++)
+                    chart1.Series[1].Points.AddXY(sm.x[i], sm.y[i]);
+
+               
+            }
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            setup();
+        }
+
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            linear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            bspline();
+        }
+
+
+    }
+}
